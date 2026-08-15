bc9321ad75f86754890f41f6bb7c4389e3fd122e
Alvaro Cuellar <alvarocuellar1995@hotmail.com>
2026-08-15 14:32:00 +0200

    Use www as canonical domain

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "humanitalis.com" }],
+        destination: "https://www.humanitalis.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
